b1c9c130b55b8919d441ddd63b37151d12e92e0f
Bruna Gil <bruna.gil@unifesp.br>
2021-05-12 22:03:39 -0300

    add comentarios e cria script para exercicios map, reduce e filter

--- exercicio-sabado/bloco-2/script.js
+++ exercicio-sabado/bloco-2/script.js
@@ -2,15 +2,15 @@
 
 // 1. Crie uma array com 5 frutas diferentes;
 
-const frutas = ["banana", "pera", "morango", "kiwi", "manga"]
+const frutas = ["banana", "pera", "morango", "kiwi", "manga"];
 
 //2. Crie uma array com 100 espaços vazios.
 
-const arrayCem = new Array(100) 
+const arrayCem = new Array(100);
 
-// slots = espaços 
-arrayCem[2] //undefined
-arrayCem[10] // undefined 
+// slots = espaços
+arrayCem[2]; //undefined
+arrayCem[10]; // undefined
 
 //exemplo da aluna <3
 let posicoes = [];
@@ -18,7 +18,7 @@ posicoes.length = 100;
 
 //3. Printe o ultimo elemento da array de frutas, baseando-se no seu length
 
-console.log(frutas[frutas.length - 1])
+console.log(frutas[frutas.length - 1]);
 // frutas[5 - 1]
 // frutas[4]
 
@@ -26,20 +26,20 @@ console.log(frutas[frutas.length - 1])
 
 //4. Printar na tela em cada linha o nome de uma fruta da array anterior;
 
-for(let i=0; i < frutas.length; i++) {
-    console.log(frutas[i])
-  }
-  //output: banana, pera, morango, kiwi, manga
+for (let i = 0; i < frutas.length; i++) {
+  console.log(frutas[i]);
+}
+//output: banana, pera, morango, kiwi, manga
 
-//exemplo da aluna <3  
-for(i of frutas) {
-  console.log(i)
+//exemplo da aluna <3
+for (i of frutas) {
+  console.log(i);
 }
 
 //5. Inserir números crescentes a partir do 1 na array de 100 slots (a array vai ter 100 items, com o primeiro sendo 1 e o último sendo 100).
 
-for(let i=0; i < arrayCem.length; i++) {
-  arrayCem[i] = i + 1
+for (let i = 0; i < arrayCem.length; i++) {
+  arrayCem[i] = i + 1;
 }
 
 //----- ForEach
@@ -48,78 +48,11 @@ for(let i=0; i < arrayCem.length; i++) {
 
 arrayCem.forEach((item) => {
   console.log(item * 2);
-})
-
-// (a, b) => 
-// function(a, b) 
+});
 
 //7. Dada a seguinte array, printe o tamanho de cada string a cada iteração
 
 const elementosQuimicos = ["Hidrogenio", "Helio", "Litio", "Oxigenio"];
 
-//---- Map
 
-//8. Dada uma array de nomes, criar uma nova array que adiciona o pronome de tratamento "sra." antes de cada nome;
-
-//9. Dado uma array de preços de livros, retorne uma array com os preços com desconto de 20%.
-
-
-//---- Filter **
-
-//10. Dada a array de livros *com desconto*, crie uma nova array contendo todos os preços que estão abaixo de 30;
-
-
-//---- Reduce ** 
-
-//11. Como essa promoção de livros está muito boa e você tem dinheiro para comprar os livros que quer, some os preços com desconto dessa array
-
-
-//---- Objetos
-
-//12. Crie um objeto chamado animal com as seguintes propriedades: porte, especie e raca; coloque os valores que quiser
-
-
-//13. Crie um objeto curso que receba as propriedades: nome, período, professora. Atribua valores as propriedades criadas e separadamente mostre as propriedades no console.log
-
-
-//--- tudo junto e misturado!!
-
-//. Dada a seguinte array de objetos:
-
-const data = [
-  {
-    nome: "Scooby Doo",
-    idade: 7,
-    tipo: "cachorro",
-  },
-  {
-    nome: "Sofia",
-    idade: 1,
-    tipo: "gato",
-  },
-  {
-    nome: "Bidu",
-    idade: 6,
-    tipo: "cachorro",
-  },
-  {
-    nome: "Mimi",
-    idade: 3,
-    tipo: "gato",
-  },
-  {
-    nome: "Alfredo",
-    idade: 10,
-    tipo: "gato",
-  },
-
-];
-
-// printar no console apenas os cachorros
-
-// printe no console apenas os gatos
-
-// some a idade de todos os gatos juntos
-
-
-
+//os três principais métodos de array estão em outro documento!
