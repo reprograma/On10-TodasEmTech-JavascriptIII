8c738b44cd1a1a16433a9c1164c987d24bd911c0
Izabelle Souza <izabellepsouzaa@gmail.com>
2021-05-14 22:59:10 -0300

    atualizando... .

--- exercicio-sabado/exercicio-casa-izabelle/Exercicio URI 02/script.js
+++ exercicio-sabado/exercicio-casa-izabelle/Exercicio URI 02/script.js
@@ -1,39 +1,40 @@
 // - [Exercício 02](https://www.urionlinejudge.com.br/judge/pt/problems/view/1049) //
 
-const tipoVertebra = prompt("É vertebrado ou invertebrado?")
+function setAnimalType(tipoVertebra) {
+  if (tipoVertebra === "vertebrado") {
+    const tipoAnimal = prompt("É ave ou mamífero")
 
-if(tipoVertebra == "vertebrado") {
-  const tipoAnimal = prompt("É ave ou mamifero")
-    if(tipoAnimal == "ave") {
+    if (tipoAnimal === "ave") {
       const tipoComida = prompt("É carnivoro ou onivoro?")
-        if(tipoComida == "carnivoro") {
-          alert("É uma aguia")
-        } else {
-          alert("É uma pomba")
-        }
-    } else {
-      const tipoComida = prompt("É onivoro ou herbivoro?")
-        if(tipoComida == "onivoro") {
-          alert("É um homem")
-        } else {
-          alert("É uma vaca")
-        }
-    } 
-} else {  
-  const tipoAnimal = prompt("É inseto ou anelideo")
-    if(tipoAnimal == "inseto"){
-      const tipoComida = prompt("É hematofago ou herbivoro?")
-        if(tipoComida == "hematofago") {
-          alert("É uma pulga")
-        } else {
-          alert("É uma lagarta")
-        }
-    } else {
-      const tipoComida = prompt("É hematofago ou onivoro?")
-        if(tipoComida == "hematofago") {
-          alert("É uma sanguessuga")
-        } else {
-          alert("É uma minhoca")
-        }
+      tipoComida === "carnivoro" && alert("É uma aguia")
+      tipoComida === "onivoro" && alert("É uma pomba")
+      return
     }
+
+    if (tipoAnimal === "mamífero"){
+      const tipoComida = prompt("É onivoro ou herbivoro?")
+      tipoComida === "onivoro" && alert("É um homem")
+      tipoComida === "herbivoro" && alert("É uma vaca")
+      return
+    }
+  }
+
+  if (tipoVertebra === "invertebrado")  {
+    const tipoAnimal = prompt("É inseto ou anelideo")
+
+    if (tipoAnimal === "inseto") {
+      const tipoComida = prompt("É hematofago ou herbivoro?")
+      tipoComida === "hematofago" && alert("É uma pulga")
+      tipoComida === "herbivoro" && alert("É uma lagarta")
+      return
+    }
+
+    if (tipoAnimal === "anelideo") {
+      const tipoComida = prompt("É hematofago ou onivoro?")
+      tipoComida === "hematofago" && alert("É uma sanguessuga")
+      tipoComida === "onivoro" && alert("É uma minhoca")
+      return
+    }
+  }
 }
+setAnimalType(prompt("É vertebrado ou invertebrado?"))
