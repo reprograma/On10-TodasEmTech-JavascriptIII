f42bd9128890eaeacf585bece47fb291be6132e6
Rosalyne Duarte <rosalyneduarte@gmail.com>
2021-05-14 21:16:01 -0300

    rosalyneDuarte, Exercicio Semanal

--- exercicio-sabado/exercicio-casa/script.js
+++ exercicio-sabado/exercicio-casa/script.js
@@ -15,6 +15,24 @@ const users = [
   },
 ]
 
+// Resolução, Rosalyne
+let newUsers = [];
+for (let i=0; i < users.length; i++) {
+  
+  let user = users[i];
+  let fullNameNew = user.fullName.firstName + " " + user.fullName.lastName;
+
+  let newObject = {
+    fullName : fullNameNew,
+    genres: user.genres,
+    score: user.score
+  }
+
+  newUsers.push(newObject);
+}
+
+console.log(newUsers);
+
 /*
   Você recebeu uma array de objetos da nova desenvolvedora backend do seu trabalho dos sonhos. Porém, percebeu que não vai precisar de todas essas informações! Por conta disso, será necessário criar um novo objeto apenas com aquilo que será utilizado
 
@@ -34,4 +52,18 @@ const users = [
     score: 153
 
   }
+*/
+
+/* RESOLUÇÕES DAS QUESTÔES ABERTAS
+
+1. O que é iteração?
+
+Repetição de uma ação, como por exemplo: pega a função e faz um for com a condição (A cada elemento do meu Array 
+multiplica, por 2 ), cada repetição, seria uma interação seria.
+
+2. Qual a diferença de uma function padrão e a arrow function?
+
+O Arrow Function é a forma mais recente de escrever uma função regular (função normal), sendo no formato mais 
+clara e concisa
+
 */
---
+++ rosalyneDuarte ... sabado/bloco-2/exercicio1.js
@@ -0,0 +1,10 @@
+// 1. Crie uma array com 5 frutas diferentes;
+
+let frutas = [
+    'abacate',
+    'melao',
+    'banana',
+    'laranja',
+    'pera'
+]
+console.log(frutas.length)
